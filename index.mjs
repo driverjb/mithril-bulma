a33003c70a46be501df90eb0a13c6e97c14b3101
driverjb <driverjb09@gmail.com>
2025-10-31 12:04:13 -0400

    corrected autocomplete for top level exports

--- index.mjs
+++ index.mjs
@@ -1,44 +1,45 @@
-import * as Column from './columns/index.mjs';
-import * as Component from './components/index.mjs';
-import * as Element from './elements/index.mjs';
-import * as Form from './form/index.mjs';
-import * as Grid from './grid/index.mjs';
-import * as Layout from './layout/index.mjs';
+import * as C from './columns/index.mjs';
+import * as Cp from './components/index.mjs';
+import * as E from './elements/index.mjs';
+import * as F from './form/index.mjs';
+import * as G from './grid/index.mjs';
+import * as L from './layout/index.mjs';
 
-export default {
-  /**
-   * The power of Flexbox in a simple interface
-   *
-   * https://bulma.io/documentation/columns/
-   */
-  Column,
-  /**
-   * Advanced multi-part components with lots of possibilities
-   *
-   * https://bulma.io/documentation/components/
-   **/
-  Component,
-  /**
-   * Essential interface elements that only require a single CSS class
-   *
-   * https://bulma.io/documentation/elements/ */
-  Element,
-  /**
-   * The indispensable form controls, designed for maximum clarity
-   *
-   * https://bulma.io/documentation/form/
-   */
-  Form,
-  /**
-   * A 2 dimensional layout component
-   *
-   * https://bulma.io/documentation/grid/
-   */
-  Grid,
-  /**
-   * Design the structure of your webpage with these CSS classes
-   *
-   * https://bulma.io/documentation/layout/
-   */
-  Layout
-};
+/**
+ * The power of Flexbox in a simple interface
+ *
+ * https://bulma.io/documentation/columns/
+ */
+const Column = C;
+
+/**
+ * Advanced multi-part components with lots of possibilities
+ *
+ * https://bulma.io/documentation/components/
+ **/
+const Component = Cp;
+/**
+ * Essential interface elements that only require a single CSS class
+ *
+ * https://bulma.io/documentation/elements/ */
+const Element = E;
+/**
+ * The indispensable form controls, designed for maximum clarity
+ *
+ * https://bulma.io/documentation/form/
+ */
+const Form = F;
+/**
+ * A 2 dimensional layout component
+ *
+ * https://bulma.io/documentation/grid/
+ */
+const Grid = G;
+/**
+ * Design the structure of your webpage with these CSS classes
+ *
+ * https://bulma.io/documentation/layout/
+ */
+const Layout = L;
+
+export default { Column, Component, Element, Form, Grid, Layout };
--- types/index.d.mts
+++ types/index.d.mts
@@ -7,9 +7,44 @@ declare namespace _default {
     export { Layout };
 }
 export default _default;
-import * as Column from './columns/index.mjs';
-import * as Component from './components/index.mjs';
-import * as Element from './elements/index.mjs';
-import * as Form from './form/index.mjs';
-import * as Grid from './grid/index.mjs';
-import * as Layout from './layout/index.mjs';
+/**
+ * The power of Flexbox in a simple interface
+ *
+ * https://bulma.io/documentation/columns/
+ */
+declare const Column: typeof C;
+/**
+ * Advanced multi-part components with lots of possibilities
+ *
+ * https://bulma.io/documentation/components/
+ **/
+declare const Component: typeof Cp;
+/**
+ * Essential interface elements that only require a single CSS class
+ *
+ * https://bulma.io/documentation/elements/ */
+declare const Element: typeof E;
+/**
+ * The indispensable form controls, designed for maximum clarity
+ *
+ * https://bulma.io/documentation/form/
+ */
+declare const Form: typeof F;
+/**
+ * A 2 dimensional layout component
+ *
+ * https://bulma.io/documentation/grid/
+ */
+declare const Grid: typeof G;
+/**
+ * Design the structure of your webpage with these CSS classes
+ *
+ * https://bulma.io/documentation/layout/
+ */
+declare const Layout: typeof L;
+import * as C from './columns/index.mjs';
+import * as Cp from './components/index.mjs';
+import * as E from './elements/index.mjs';
+import * as F from './form/index.mjs';
+import * as G from './grid/index.mjs';
+import * as L from './layout/index.mjs';
